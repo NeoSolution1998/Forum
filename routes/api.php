42d4d2ed260f975fd08809c1a21ec7a4ec71240d
NeoSolution1998 <neosolution1998@gmail.com>
2024-08-09 05:54:26 +0300

    start

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\Api\V1\Auth\AuthController;
+use App\Http\Controllers\Api\V1\Auth\EmailVerificationController;
+use App\Http\Controllers\Api\V1\Auth\PasswordController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Auth routes
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// Password routes
+Route::post('password/reset-link', [PasswordController::class, 'sendResetLink']);
+Route::post('password/reset', [PasswordController::class, 'resetPassword']);
+Route::put('password', [PasswordController::class, 'updatePassword'])->middleware('auth:sanctum');
+
+// Email Verification routes
+Route::post('email/verification-link', [EmailVerificationController::class, 'sendVerificationLink'])->middleware('auth:sanctum');
+Route::post('email/verify', [EmailVerificationController::class, 'verifyEmail'])->middleware('auth:sanctum');
+
